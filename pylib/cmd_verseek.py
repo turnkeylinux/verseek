d64a61c52b98cc01b6b1c66a7e7ebf4b1eef1163
Liraz Siri <liraz@turnkeylinux.org>
2010-10-01 20:48:57 +0200

    support regular directories

--- pylib/cmd_verseek.py
+++ pylib/cmd_verseek.py
@@ -7,6 +7,7 @@ Options:
   -l    list seekable versions
   
 """
+from os.path import *
 import sys
 import help
 import getopt
@@ -22,6 +23,21 @@ def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
+def list(path):
+    try:
+        versions = verseek.list(path)
+    except verseek.Error, e:
+        fatal(e)
+        
+    for version in versions:
+        print version
+
+def seek(path, version):
+    try:
+        verseek.seek(path, version)
+    except verseek.Error, e:
+        fatal(e)
+    
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "l")
@@ -32,19 +48,22 @@ def main():
         usage()
 
     srcpath = args[0]
+    if not isdir(srcpath):
+        fatal("no such directory `%s'" % srcpath)
+        
     for opt, val in opts:
         if opt == '-h':
             usage()
 
         if opt == '-l':
-            return verseek.list(srcpath)
-
+            return list(srcpath)
+            
     try:
         version = args[1]
     except IndexError:
         version = None
 
-    verseek.seek(srcpath, version)
+    seek(srcpath, version)
     
 if __name__=="__main__":
     main()
--- pylib/verseek.py
+++ pylib/verseek.py
@@ -1,5 +1,26 @@
+import re
+from os.path import *
+
+class Error(Exception):
+    pass
+
+def deb_get_version(srcpath):
+    changelogfile = join(srcpath, "debian/changelog")
+    if not exists(changelogfile):
+        raise Error("no such file or directory `%s'" % changelogfile)
+
+    for line in file(changelogfile).readlines():
+        m = re.match('^\w[-+0-9a-z.]* \(([^\(\) \t]+)\)(?:\s+[-+0-9a-z.]+)+\;',line
+, re.I)
+        if m:
+            return m.group(1)
+
+    raise Error("can't parse version from `%s'" % changelogfile)
+
 def list(path):
-    print "list: " + path
+    return [ deb_get_version(path) ]
 
 def seek(path, version=None):
-    print "seek path=%s version=%s" % (path, version)
+    if version and deb_get_version(path) != version:
+        raise Error("can't seek to nonexistent version `%s'" % version)
+
