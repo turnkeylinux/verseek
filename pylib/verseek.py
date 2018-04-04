d77be6aa9f7fa1b633a21ca5da09fc7af1b8c8cc
Stefan Davis <stefan@turnkeylinux.org>
2018-04-03 17:49:36 -0800

    Fixes debian/control parsing quirks
    Now handles non-setting constructs and wrapped lines within debian/control
    files. However this does NOT make verseek truly parse debian/control files
    properly.

--- pylib/verseek.py
+++ pylib/verseek.py
@@ -208,7 +208,7 @@ class GitSingle(Git):
                      if not line.startswith(" "))
             return dict([ re.split("\s*:\s*", line, 1)
                           for line in lines
-                          if line ])
+                          if line or ':' in line])
 
         control = parse_control(self.path_control)
 
